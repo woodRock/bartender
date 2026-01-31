33d4f53766f7d680d495c63abb9654626fc660e4
woodRock <j.r.h.wood98@gmail.com>
2026-02-01 05:39:51 +1300

    Paper dockets

--- scenes/order_docket.gd
+++ scenes/order_docket.gd
@@ -1,39 +1,42 @@
 extends PanelContainer
 
-signal order_expired(docket_node)
+signal order_expired(docket)
 
-@onready var drink_label = $VBoxContainer/DrinkLabel
-@onready var ingredients_label = $VBoxContainer/IngredientsLabel
-@onready var timer_bar = $VBoxContainer/ProgressBar
-# Ensure this path matches your actual Scene Tree exactly!
-@onready var timer = $VBoxContainer/Timer 
+@onready var name_label = $MarginContainer/VBoxContainer/DrinkLabel
+@onready var ingredients_label = $MarginContainer/VBoxContainer/IngredientsLabel
+@onready var timer_bar = $MarginContainer/VBoxContainer/TimerProgressBar
 
-var recipe_data: Dictionary
+var recipe_data = {}
+var max_time: float
+var current_time: float
 
-func setup(drink_name: String, ingredients: Array, time_limit: float):
-	recipe_data = {"name": drink_name, "ingredients": ingredients}
+func setup(d_name: String, ingredients: Array, time_limit: float):
+	recipe_data = {"name": d_name, "ingredients": ingredients}
+	max_time = time_limit
+	current_time = time_limit
 	
-	# Safety check: if nodes aren't ready yet, this avoids 'null' errors
-	if not is_inside_tree(): await ready 
+	# Header styling
+	name_label.text = d_name.to_upper()
 	
-	drink_label.text = drink_name
-	ingredients_label.text = format_ingredients(ingredients)
-	
-	timer.wait_time = time_limit
-	timer_bar.max_value = time_limit
-	timer_bar.value = time_limit
-	timer.start()
-
-func _process(_delta):
-	if timer.time_left > 0:
-		timer_bar.value = timer.time_left
-
-func _on_timer_timeout():
-	order_expired.emit(self)
-	queue_free()
-
-func format_ingredients(ingredients: Array) -> String:
+	# Receipt-style list
 	var list_text = ""
 	for item in ingredients:
-		list_text += "- " + str(item) + "\n" # str() ensures it doesn't crash if an item isn't a string
-	return list_text.strip_edges()
+		list_text += "• " + item + "\n"
+	ingredients_label.text = list_text
+	
+	if timer_bar:
+		timer_bar.max_value = max_time
+		timer_bar.value = max_time
+
+func _process(delta):
+	current_time -= delta
+	if timer_bar:
+		timer_bar.value = current_time
+	
+	# Visual urgency: change bar color
+	if current_time < max_time * 0.25:
+		timer_bar.modulate = Color.DARK_RED
+		
+	if current_time <= 0:
+		order_expired.emit(self)
+		queue_free()
